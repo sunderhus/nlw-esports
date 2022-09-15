97220794ec3ed37d24c34ff7afb06bb0f27a5c0b
Sunderhus <matheus.sunderhus@gmail.com>
2022-09-15 19:21:09 -0300

    chore: add instambul provider to vitest configuration

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
+    coverage:{
+      provider:'istanbul'
+    },
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: true,

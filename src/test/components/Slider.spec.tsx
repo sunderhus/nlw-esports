01094d5add75df6f92e81a2d37853f05a0aa9f58
Sunderhus <matheus.sunderhus@gmail.com>
2022-10-08 17:15:12 -0300

    test: ensure to present six cards per slide

--- src/test/components/Slider.spec.tsx
+++ src/test/components/Slider.spec.tsx
@@ -1,16 +1,25 @@
 import { Slider } from '@/components'
+import { ThemeSelectorProvider } from '@/contexts/themeProvider/ThemeSelectorProvider'
 import { render, screen } from '@testing-library/react'
 
-const makeSut = () => render(<Slider />);
-
+const makeSut = () => {
+    const sut = render(
+        <ThemeSelectorProvider>
+            {<Slider />}
+        </ThemeSelectorProvider>
+    )
+    return {
+        sut,
+    }
+}
 describe('Slider', () => {
-    it('should present a card game image', () => {
+
+    it('should present a maximum of six cards per slide', () => {
         makeSut();
 
-        const cardImage = screen.getByRole('img')
+        const cardImage = screen.getAllByRole('img')
 
-        expect(cardImage).toBeInTheDocument();
+        expect(cardImage).toHaveLength(6);
     })
-
 
 })
